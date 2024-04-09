8c288265a79902f3d760411d9665039d35f86303
Sonal Thirani <sthirani@horizon.csueeastbay.edu>
2024-04-09 01:32:04 -0700

    Completed CC-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,44 @@
+// Find Missing Number
 
+// Time Complexity: O(logN), Space Complexity O(1)
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+      if(nums[0] != 0) return 0;
+      if(nums[nums.length - 1] != nums.length) return nums.length;
+        //[0,1,3]
+        //[0,1,2,3,4,5,6,7,9]
+        // 0,1,2,3,4,5,6,7,8
+
+        //[0,2,3]
+        // 0,1,2
+        //[0,1,3,4,5,6,7,8,9]
+        // 0,1,2,3,4,5,6,7,8
+        int low = 0; int high = nums.length-1;
+
+        while(low<=high){
+        
+        
+        int mid = low + (high-low)/2;
+
+            // mid is missing num start to miss
+            if(nums[mid]!=mid){
+                if(mid==0||nums[mid-1]==mid-1){
+                    return mid;
+                } else {
+                    high = mid-1;
+                }
+            }
+            // right
+            else if(nums[high]!=high){
+                low = mid+1;
+            }
+            //left
+            else if (nums[low]!=low){
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,87 @@
 
+    
+// Design Min Heap
+
+/* 
+Heap is complete binary tree with heap property.
+DS used is an array.
+If k is a parent - left node is 2k and right node is 2k+1
+
+
+Functions we want:  parent, leftchild, rightchild, isLeaf, size, heapify, insert, get, remove
+*/ 
+
+public class Problem2{
+    int[] heap;
+    int capacity;
+    int size;
+
+    Problem2(){
+        this.size = 0;
+        this.capacity = 10;
+        this.heap = new int[capacity];
+    }
+
+    private int parent(int index){
+        return index/2;
+    }
+    private int leftChild(int index){
+        return (2*index);
+    }
+    private int righChild(int index){
+        return (2*index+1);
+    }
+
+    private boolean isLeaf(int index){
+        // All leaf nodes are the ones wih indexes > no.of.current.nodes/2;
+        return index>(size/2) && index<=size;
+    }
+
+    private void swap(int p1, int p2){
+        int temp = heap[p1];
+        heap[p1]=heap[p2];
+        heap[p2]=temp;
+    }
+    public int get (){
+        return heap[1];
+    }
+    public void insert(int value){
+        if(size>=capacity) return;
+        // first add element at the end to maintain complete binary tree
+        heap[++size]=value;
+        int current = size;
+        
+        while(heap[parent(current)]>heap[current]){
+            swap(parent(current), current);
+            current = parent(current);
+        }
+        
+    }
+    public int remove(){
+        int removed = heap[1];
+        heap[1]=heap[size--];
+        minHeapify(1);
+        return removed;
+
+    }
+    public void minHeapify(int pos){
+        if(!isLeaf(pos)){
+            int left = leftChild(pos);
+            int right = righChild(pos);
+            int smallest = pos;
+            if(left<size && heap[left]<heap[smallest]){
+                smallest = left;
+            }
+            if(right<size && heap[right]<heap[smallest]){
+                smallest = right;
+            }
+            
+            if(smallest !=pos){
+                swap(smallest,pos);
+                minHeapify(smallest);
+        
+            }
+        }
+        
+    }
+}
